c436a41850f0e576f4ebc8ae677d655c23415410
Evraa <evramyousef@gmail.com>
2021-07-10 03:50:08 +0200

    resume_ranking .. restructure directories

--- models/resume_ranker/dev/dev.py
+++ models/resume_ranker/dev/dev.py
@@ -40,20 +40,20 @@ resumes = Resumes('data/pdf')
 # train model
 model = Word2Vec(sentences=resumes, vector_size=100, window=5, min_count=1, workers=6, sg=1, hs=1)
 # save it
-if not os.path.exists('models'): os.mkdir('models')
-model.save("models/word2vec.model")
+if not os.path.exists('../model'): os.mkdir('../model')
+model.save("../model/word2vec.model")
 
 
 print ("Training Completed")
 
 # Store just the words + their trained embeddings.
 word_vectors = model.wv
-if not os.path.exists('models'): os.mkdir('models')
-word_vectors.save("models/word2vec.wordvectors")
+if not os.path.exists('../model'): os.mkdir('../model')
+word_vectors.save("../model/word2vec.wordvectors")
 
 # Load back with memory-mapping = read-only, shared across processes.
-# wv = KeyedVectors.load("models/word2vec.wordvectors", mmap='r')
+# wv = KeyedVectors.load("../model/word2vec.wordvectors", mmap='r')
 
-# model = Word2Vec.load("models/word2vec.model")
+# model = Word2Vec.load("../model/word2vec.model")
 # sims = model.wv.most_similar('computer', topn=10)  # get other similar words
 # print (sims)
